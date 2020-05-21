83dc3d91b8b71dc622d1112ece64a87852b19005
Ali Rahimlou <Rahimlou91@gmail.com>
2020-05-21 14:09:41 -0400

    f

--- src/index.js
+++ src/index.js
@@ -10,12 +10,8 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

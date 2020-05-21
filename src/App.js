acd094d729700521c37af1786bb3244b49ea1bb6
Ali Rahimlou <Rahimlou91@gmail.com>
2020-05-21 02:40:52 -0400

    moved nav

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React from "react";
 import HoverPic from "./components/header/header";
 import Listen from "./components/listen/listen";
 import Watch from "./components/watch/watchvideo";
-import Footer from './components/footer/footer';
+import Footer from "./components/footer/footer";
+import Navbar from "./components/navbar/navbar";
 
-
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className='div'>
+    <div className="div">
+      <Navbar />
       <HoverPic />
-      <Listen/>
-      <Watch/>
-      <Footer/>
+      <Listen />
+      <Watch />
+      <Footer />
     </div>
   );
 }

654f04eae4e7b53397f09463f62b24200c986930
Ali Rahimlou <Rahimlou91@gmail.com>
2020-05-21 02:02:00 -0400

    Updates

---
+++ precache-manifest.b3288105301e5a01a3741cd39920f661.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068ef5a345893340bb2cfa0deb64ce17",
+    "url": "/Authentic-Geezy/index.html"
+  },
+  {
+    "revision": "74074670a9f77213f851",
+    "url": "/Authentic-Geezy/static/css/2.8a7a4b9e.chunk.css"
+  },
+  {
+    "revision": "92c464e4eb86df47edd9",
+    "url": "/Authentic-Geezy/static/css/main.69be241e.chunk.css"
+  },
+  {
+    "revision": "74074670a9f77213f851",
+    "url": "/Authentic-Geezy/static/js/2.47a37bbc.chunk.js"
+  },
+  {
+    "revision": "b46eae19120e0343c25f399c83a404d7",
+    "url": "/Authentic-Geezy/static/js/2.47a37bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92c464e4eb86df47edd9",
+    "url": "/Authentic-Geezy/static/js/main.25b4c6ab.chunk.js"
+  },
+  {
+    "revision": "4b91c7be6274e6111503",
+    "url": "/Authentic-Geezy/static/js/runtime-main.80d0ed49.js"
+  },
+  {
+    "revision": "dc79b66998aad53f9e491d3d1138f1c4",
+    "url": "/Authentic-Geezy/static/media/ae-logo2.dc79b669.png"
+  },
+  {
+    "revision": "6271fe12ca506f11789b127bbc7954de",
+    "url": "/Authentic-Geezy/static/media/album-cover.6271fe12.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Authentic-Geezy/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Authentic-Geezy/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "2bef8f9d638a511fdf290af2c909efa2",
+    "url": "/Authentic-Geezy/static/media/bythecoveredit.2bef8f9d.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/Authentic-Geezy/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/Authentic-Geezy/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/Authentic-Geezy/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/Authentic-Geezy/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/Authentic-Geezy/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/Authentic-Geezy/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/Authentic-Geezy/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/Authentic-Geezy/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/Authentic-Geezy/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/Authentic-Geezy/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/Authentic-Geezy/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/Authentic-Geezy/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/Authentic-Geezy/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/Authentic-Geezy/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/Authentic-Geezy/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "7b2c1a090774d9c46dcc9b61159e1076",
+    "url": "/Authentic-Geezy/static/media/geezylogo.7b2c1a09.png"
+  },
+  {
+    "revision": "04499e8314e8122ca6c9f2e022ad87a8",
+    "url": "/Authentic-Geezy/static/media/header.04499e83.jpg"
+  }
+]);

847d7140c7a2d242e46fc28c4f12a44d4e998b9a
Ali Rahimlou <Rahimlou91@gmail.com>
2020-05-23 00:20:24 -0400

    media

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -34,19 +34,24 @@ class Navbar extends React.Component {
           <header>
             <MDBNavbar className='navbar' style={bgPink} dark expand="md" scrolling fixed="top">
               <MDBNavbarBrand >
+
                 <img className="ae-logo" src={AeLogo} alt=''></img>
                 <strong className="presents">Authentic Empire Presents</strong>
+                
                 <img className="geezy-logo" src={geezylogo} alt=''></img>
 
                 <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
                   <MDBIcon id='fb' fab icon="facebook-f" />
                 </a>
+
                 <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"> 
                   <MDBIcon id='twitter' fab icon="twitter" />
                 </a>
+
                 <a href="https://instagram.com/geezydiegoescobar?igshid=1i9upjuvrfky8" target="_blank" rel="noopener noreferrer">
                   <MDBIcon id='insta' fab icon="instagram" />
                 </a>
+
               </MDBNavbarBrand>
 
             </MDBNavbar>
